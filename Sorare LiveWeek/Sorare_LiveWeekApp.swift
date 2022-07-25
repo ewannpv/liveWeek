66fc34faf4e01398c176cbc623f38b4b634f2527
Ewann <ewannpv@google.com>
2022-07-25 21:05:40 +0200

    Initial Commit

---
+++ Sorare LiveWeek/Sorare_LiveWeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sorare_LiveWeekApp.swift
+//  Sorare LiveWeek
+//
+//  Created by Ewann Pellé on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sorare_LiveWeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sorare LiveWeekUITests/Sorare_LiveWeekUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sorare_LiveWeekUITestsLaunchTests.swift
+//  Sorare LiveWeekUITests
+//
+//  Created by Ewann Pellé on 25/07/2022.
+//
+
+import XCTest
+
+class Sorare_LiveWeekUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
